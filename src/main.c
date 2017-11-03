bb09d68e2d167df1cef699907758ef8fa516e9d6
hreintam <h2liann@gmail.com>
2017-11-03 21:16:26 +0200

    Added lab 2 code

--- src/main.c
+++ src/main.c
@@ -2,18 +2,36 @@
 #include <util/delay.h>
 
 #define BLINK_DELAY_MS 1000
+#define BLINK_DELAY_BETWEEN_LIGHTS_MS 500
 
 void main (void)
 {
-	/* Set port B pin 7 for output for Arduino Mega yellow LED */
-	DDRB |= _BV(DDB7);
+    /* Set port A pin 22, 24 and 26 for output for red, green and blue LEDs */
+    DDRA |= _BV(DDA0);
+    DDRA |= _BV(DDA2);
+    DDRA |= _BV(DDA4);
+    /* Set port B pin 7 for output and set it low */
+    DDRB |= _BV(DDB7);
+    PORTB &= ~_BV(PORTB7);
 
-	while (1) {
-		/* Set port B pin 7 high to turn Arduino Mega yellow LED on */
-		PORTB |= _BV(PORTB7);
-		_delay_ms(BLINK_DELAY_MS);
-		/* Set port B pin 7 high to turn Arduino Mega yellow LED off */
-		PORTB &= ~_BV(PORTB7);
-		_delay_ms(BLINK_DELAY_MS);
-	}
+    while (1) {
+        /* Set port A pin 22 high to turn red LED on */
+        PORTA |= _BV(PORTA0);
+        _delay_ms(BLINK_DELAY_MS);
+        /* Set port A pin 22 low to turn red LED off */
+        PORTA &= ~_BV(PORTA0);
+        _delay_ms(BLINK_DELAY_BETWEEN_LIGHTS_MS);
+        /* Set port A pin 24 high to turn green LED on */
+        PORTA |= _BV(PORTA2);
+        _delay_ms(BLINK_DELAY_MS);
+        /* Set port A pin 24 low to turn green LED off */
+        PORTA &= ~_BV(PORTA2);
+        _delay_ms(BLINK_DELAY_BETWEEN_LIGHTS_MS);
+        /* Set port A pin 26 high to turn blue LED on */
+        PORTA |= _BV(PORTA4);
+        _delay_ms(BLINK_DELAY_MS);
+        /* Set port A pin 26 low to turn blue LED off */
+        PORTA &= ~_BV(PORTA4);
+        _delay_ms(BLINK_DELAY_BETWEEN_LIGHTS_MS);
+    }
 }
